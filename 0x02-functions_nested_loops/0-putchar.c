43f57c97bc36a6d48b281aaf608e57e13589f52a
Paul_Aroo <timmypaul2006@gmail.com>
2022-03-15 12:45:38 +0100

    update: rename header file and refactor putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints 'putchar'
+ * main - prints _putchar
  *
- * Description: prints 'putchar' followed by a new line
+ * Description: prints _putchar
  *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
 
 	return (0);
 }
